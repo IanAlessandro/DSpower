ff9ae35ce811275207ccbf69203944dfe2f7435e
IanAlessandro <165028836+IanAlessandro@users.noreply.github.com>
2024-04-05 10:08:27 -0300

    Add files via upload

---
+++ atv.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+altura = input('Digite sua altura: ')
+peso = input('Digite seu peso: ')
+
+print("Olá", nome)
+print("Seja bem vindo !!!")
+print("Você tem ", idade "anos")
